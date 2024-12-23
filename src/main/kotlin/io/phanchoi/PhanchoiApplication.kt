1f68a8ed87d37446900f4155c8e72014f10c6a7e
jeongchoixx <jennachoi27@gmail.com>
2024-12-23 20:49:10 +0900

    Initial commit

---
+++ src/main/kotlin/io/phanchoi/PhanchoiApplication.kt
@@ -0,0 +1,11 @@
+package io.phanchoi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhanchoiApplication
+
+fun main(args: Array<String>) {
+	runApplication<PhanchoiApplication>(*args)
+}
---
+++ src/test/kotlin/io/phanchoi/PhanchoiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.phanchoi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhanchoiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
